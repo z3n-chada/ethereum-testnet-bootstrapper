e901dbccbafbea127704ffff02aa27917515b18a
z3n <tyler@ethereum.org>
2022-02-21 10:10:03 -0600

    pre-push

--- apps/status-check.py
+++ apps/status-check.py
@@ -1,9 +1,23 @@
 """
-    Provides a method of gettting a high level overview of the running experiment
-    to check on the health of the network.
+    Provides a method of gettting a high level overview of the running
+    experiment to check on the health of the network.
+
+    Status checking is done in 3 phases.
+
+    Phase0:
+        allow the network to come up, confirm that things are behaving properly.
+
+    Phase1:
+        runs post experiment. If there is chaos, there should be some failing
+        testcases here.
+
+    Phase2:
+        after some time has passed and the experiment is over we check to see
+        if we were able to heal.
 """
-from modules.BeaconApi import get_api_manager_from_config, APIRequest
 import time
+
+from modules.BeaconApi import APIRequest, get_api_manager_from_config
 
 # api_requests to run to comapre nodes.
 health_check_apis = {
@@ -32,7 +46,8 @@ states = ["head", "justified", "finalized"]
 
 
 def perform_experiment():
-    print("Implement me.")
+    print("Launching Experiment!")
+    # import experiment modules here.
 
 
 def check_health(manager, num_checks, check_delay):
@@ -89,6 +104,13 @@ if __name__ == "__main__":
         help="path to config file to consume for experiment",
     )
     parser.add_argument(
+        "--experiment-slot",
+        dest="experiment_slot",
+        default=64,
+        type=int,
+        help="number of slots to wait before checking initial network health",
+    )
+    parser.add_argument(
         "--phase0-slot",
         dest="phase0_slot",
         default=64,
@@ -134,14 +156,30 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    if args.phase0_slot > args.phase1_slot or args.phase0_slot > args.experiment_slot:
+        print("WARN: phase0 and phase1/experiment slots are in wrong order", flush=True)
+        print("WARN: continuing anyway...")
+
+    if args.phase1_slot > args.phase2_slot or args.phase1_slot > args.experiment_slot:
+        print("WARN: phase1 and phase2/experiment slots are in wrong order", flush=True)
+        print("WARN: continuing anyway...")
+
     manager = get_api_manager_from_config(args.config)
 
     # wait for network init.
     time.sleep(args.genesis_delay)
+    # phase0 testing.
     wait_for_slot(manager, args.phase0_slot)
     check_health(manager, args.num_checks, args.check_delay)
+
+    # start experiment
+    wait_for_slot(manager, args.experiment_slot)
     perform_experiment()
+
+    # start phase1
     wait_for_slot(manager, args.phase1_slot)
     check_health(manager, args.num_checks, args.check_delay)
+
+    # start phase2
     wait_for_slot(manager, args.phase2_slot)
     check_health(manager, args.num_checks, args.check_delay)
