36c4f524cb3f1fac3cf574616183e593ba06c859
z3n <tyler@ethereum.org>
2022-02-23 09:18:20 -0600

    updated configs for deposits

--- apps/deploy-contract-deposits.py
+++ apps/deploy-contract-deposits.py
@@ -1,5 +1,10 @@
 """
-    Given CL slot times, deploy transactions to and from wallets.
+    Given EL slot times, deploy transactions to and from wallets.
+
+    Optionally allow the payments to be broken up and scattered.
+
+    NOTE: this module is the only one that uses the execution layer for
+    checks to time.
 """
 import pathlib
 import random
@@ -12,7 +17,7 @@ from web3.auto import w3
 w3.eth.account.enable_unaudited_hdwallet_features()
 
 
-def get_get_keypairs(config):
+def get_keypairs(config):
     k = {}
     mnemonic = config["accounts"]["eth1-account-mnemonic"]
     password = config["accounts"]["eth1-passphrase"]
@@ -26,9 +31,7 @@ def get_get_keypairs(config):
     return k
 
 
-def create_deposit_data(config, args):
-    start_offset = args.start_offset
-    num_deposits = args.num_deposits
+def create_deposit_data(config, start_offset, num_deposits, amount):
     chain_id = config["config-params"]["deposit-chain-id"]
     validator_mnem = config["accounts"]["validator-mnemonic"]
     withdrawl_mnem = config["accounts"]["withdrawl-mnemonic"]
@@ -39,11 +42,12 @@ def create_deposit_data(config, args):
         + f"--fork-version {chain_id} "
         + f'--validators-mnemonic "{validator_mnem}" '
         + f'--withdrawals-mnemonic "{withdrawl_mnem}" '
+        + f"--amount {amount} "
         + "--as-json-list "
     )
-    print(cmd)
     x = subprocess.run(cmd, shell=True, capture_output=True)
     stdout_deposit_data = x.stdout
+    print(stdout_deposit_data, flush=True)
     return stdout_deposit_data
 
 
@@ -63,35 +67,45 @@ def send_ethereal_beacon_deposit(deposit_address, data, pubkey, privkey, conn):
     print(subprocess.run(cmd, shell=True, capture_output=True))
 
 
-def deploy_deposit(args):
+def deploy_deposits(args):
     with open(args.config, "r") as f:
         gc = yaml.safe_load(f.read())
 
-    deposit_data = create_deposit_data(gc, args)
-    key_pairs = get_get_keypairs(gc)
+    key_pairs = get_keypairs(gc)
     deposit_address = gc["config-params"]["deposit-contract-address"]
+    # we can optionally break up the deposit into more than one transaction
+    for t in range(args.num_transactions):
+        amount = args.deposit_size / args.num_transactions
+        deposit_data = create_deposit_data(
+            gc, args.start_offset, args.num_deposits, amount
+        )
 
-    # wrap ethereal with global_config.
-    pubkey = random.choice(list(key_pairs.keys()))
-    priv_key = key_pairs[pubkey]
-    print(f"{pubkey} : {priv_key}")
+        # wrap ethereal with global_config.
+        pubkey = random.choice(list(key_pairs.keys()))
+        priv_key = key_pairs[pubkey]
+        print(
+            f"sending {amount} for {args.num_deposits} with keys: {pubkey}-{priv_key}",
+            flush=True,
+        )
 
-    with open("/tmp/deposit_data", "wb") as f:
-        f.write(deposit_data)
+        with open("/tmp/deposit_data", "wb") as f:
+            f.write(deposit_data)
 
-    # wait for the connection to exist.
-    connection = pathlib.Path(args.geth_ipc)
-    while not connection.exists():
-        print("Waiting for geth ipc connection to open")
-        time.sleep(1)
+        # wait for the connection to exist.
+        connection = pathlib.Path(args.geth_ipc)
+        while not connection.exists():
+            print("Waiting for geth ipc connection to open")
+            time.sleep(1)
 
-    send_ethereal_beacon_deposit(
-        deposit_address,
-        "/tmp/deposit_data",
-        pubkey,
-        priv_key,
-        args.geth_ipc,
-    )
+        send_ethereal_beacon_deposit(
+            deposit_address,
+            "/tmp/deposit_data",
+            pubkey,
+            priv_key,
+            args.geth_ipc,
+        )
+
+        time.sleep(args.delay_per_transaction)
 
 
 if __name__ == "__main__":
@@ -123,8 +137,29 @@ if __name__ == "__main__":
         dest="num_deposits",
         default=3,
         type=int,
-        help="min source to pass to generate deposit-data",
+        help="number of deposits to make.",
+    )
+    parser.add_argument(
+        "--num-transactions",
+        dest="num_transactions",
+        default=1,
+        type=int,
+        help="how many transactions to split the payment over",
+    )
+    parser.add_argument(
+        "--delay-per-transaction",
+        dest="delay_per_transaction",
+        default=0,
+        type=int,
+        help="how long to sleep per transaction",
+    )
+    parser.add_argument(
+        "--total-deposit-size",
+        dest="deposit_size",
+        default=32000000000,
+        type=int,
+        help="total value of the deposits. In GWEI",
     )
 
     args = parser.parse_args()
-    deploy_deposit(args)
+    deploy_deposits(args)
