9837adcae070c8e5530ecad83b2152453e6e8950
z3n <tyler@ethereum.org>
2022-03-16 12:35:59 +0100

    new configs and clean up for merge

--- apps/launchers/fuzzers/launch-tx-fuzzer.sh
+++ apps/launchers/fuzzers/launch-tx-fuzzer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-env_vars=("CONFIG_FILE" "BOOTNODE_ENR" "EXECUTION_DATA_DIR" "EXECUTION_GENESIS" "NETWORK_ID" "EXECUTION_P2P_PORT" "EXECUTION_HTTP_PORT" "EXECUTION_WS_PORT" "HTTP_APIS" "WS_APIS" "IP_ADDR" "NETRESTRICT_RANGE" "END_FORK")
-
+env_vars=("CONFIG_FILE" "PRESET_BASE" "START_FORK" "END_FORK" "DEBUG_LEVEL" "TESTNET_DIR" "NODE_DIR" "HTTP_WEB3_IP_ADDR" "EXECUTION_HTTP_PORT" "IP_ADDR" "CONSENSUS_P2P_PORT" "BEACON_API_PORT" "BEACON_METRIC_PORT")
 for var in "${env_vars[@]}" ; do
     if [[ -z "$var" ]]; then
         echo "$var not set"
@@ -9,20 +7,12 @@ for var in "${env_vars[@]}" ; do
     fi
 done
 
-mkdir -p EXECUTION_DATA_DIR
-
-if [[ -n "$EXECUTION_LAUNCHER" ]]; then
-    echo "tx-fuzzer launching execution client"
-    ls "/data/"
-    ls "/data/local_testnet/execution-bootstrapper/"
-    "$EXECUTION_LAUNCHER" &
-fi
+/source/apps/launchers/consensus-clients/launch-teku.sh & 
 
 private_keys=`python3 /source/apps/launchers/fuzzers/tx-fuzzer.py --config "$CONFIG_FILE" --action privateKeys`
-
 public_keys=`python3 /source/apps/launchers/fuzzers/tx-fuzzer.py --config "$CONFIG_FILE" --action publicKeys`
-echo "$private_keys"
-echo "$public_keys"
+echo "tx-fuzzer using private-keys: $private_keys"
+echo "tx-fuzzer using pub-keys: $public_keys"
 
 sleep 60
 ./tx-fuzz.bin "http://$IP_ADDR:$EXECUTION_HTTP_PORT" spam "$private_keys" "$public_keys"
