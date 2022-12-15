5c648f538d6cb856fcda4520e2789908d45bf8ad
z3n <tyler@ethereum.org>
2022-12-15 13:39:32 -0600

    starting to remove erigon code for simplicity.

--- apps/modules/ExecutionBootstrapper.py
+++ apps/modules/ExecutionBootstrapper.py
@@ -37,71 +37,6 @@ class ETBExecutionBootstrapper(object):
                     jwt_secret_file = ec.get("jwt-secret-file", node)
                     with open(jwt_secret_file, "w") as f:
                         f.write(jwt_secret)
-
-    def create_erigon_nodekey_files(self):
-        for name, ec in self.etb_config.get("execution-clients").items():
-            if ec.get("client") == "erigon":
-                for node in range(ec.get("num-nodes")):
-                    with open(
-                        f"{ec.get_execution_data_dir(node)}/nodekey.txt", "w"
-                    ) as f:
-                        f.write(f"{hex(random.getrandbits(32*8))[2:].zfill(64)}")
-
-        for name, cc in self.etb_config.get("consensus-clients").items():
-            if cc.has("local-execution-client") and cc.get("local-execution-client"):
-                for node in range(cc.get("num-nodes")):
-                    if cc.execution_config.get("client") == "erigon":
-                        with open(
-                            f"{cc.get_execution_data_dir(node)}/nodekey.txt", "w"
-                        ) as f:
-                            f.write(f"{hex(random.getrandbits(32*8))[2:].zfill(64)}")
-
-    def get_erigon_enodes(self):
-        enodes = []
-        for name, ec in self.etb_config.get("execution-clients").items():
-            if ec.get("client") == "erigon":
-                for node in range(ec.get("num-nodes")):
-                    with open(
-                        f"{ec.get_execution_data_dir(node)}/nodekey.txt", "r"
-                    ) as f:
-                        nodekey = f.read()
-                    cmd = [
-                        "bootnode",
-                        "--nodekeyhex",
-                        f"{nodekey}",
-                        "--writeaddress",
-                    ]
-
-                    out = subprocess.run(cmd, capture_output=True)
-                    enode = out.stdout.decode("utf-8").strip()
-                    ip = cc.get("ip-addr", node)
-                    p2p_port = cc.execution_config.get("execution-p2p-port")
-                    enodes.append(f"{enode}@{ip}:{p2p_port}")
-
-        for name, cc in self.etb_config.get("consensus-clients").items():
-            if cc.has("local-execution-client") and cc.get("local-execution-client"):
-                for node in range(cc.get("num-nodes")):
-                    if cc.execution_config.get("client") == "erigon":
-                        with open(
-                            f"{cc.get_execution_data_dir(node)}/nodekey.txt", "r"
-                        ) as f:
-                            nodekey = f.read()
-
-                        # turn the nodekey into enode
-                        cmd = [
-                            "bootnode",
-                            "--nodekeyhex",
-                            f"{nodekey}",
-                            "--writeaddress",
-                        ]
-
-                        out = subprocess.run(cmd, capture_output=True)
-                        enode = out.stdout.decode("utf-8").strip()
-                        ip = cc.get("ip-addr", node)
-                        p2p_port = cc.execution_config.get("execution-p2p-port")
-                        enodes.append(f"enode://{enode}@{ip}:{p2p_port}")
-        return enodes
-
     def write_all_execution_genesis_files(self):
         """
         Here we create the genesis.json files that the execution
@@ -124,10 +59,3 @@ class ETBExecutionBootstrapper(object):
         nethermind_genesis = egw.create_nethermind_genesis()
         with open(nethermind_genesis_path, "w") as f:
             json.dump(nethermind_genesis, f)
-
-        # nethermind is picky..
-        # add erigon genesis for new fields for their genesis.
-        erigon_genesis_path = self.etb_config.get("erigon-genesis-file")
-        erigon_genesis = egw.create_erigon_genesis()
-        with open(erigon_genesis_path, "w") as f:
-            json.dump(erigon_genesis, f)
--- apps/modules/ExecutionGenesis.py
+++ apps/modules/ExecutionGenesis.py
@@ -72,52 +72,6 @@ class ExecutionGenesisWriter(object):
 
         return self.genesis
 
-    def create_erigon_genesis(self):
-
-        raise Exception("Currently not supported.")
-        # self.genesis = {
-        #     "alloc": self.get_allocs(),
-        #     "coinbase": "0x0000000000000000000000000000000000000000",
-        #     "difficulty": "0x01",
-        #     "extraData": "",
-        #     "gasLimit": "0x400000",
-        #     "nonce": "0x1234",
-        #     "mixhash": "0x" + ("0" * 64),
-        #     "parentHash": "0x" + ("0" * 64),
-        #     "timestamp": str(self.execution_genesis),
-        # }
-        #
-        # config = {
-        #     "chainId": self.etb_config.get("chain-id"),
-        #     "homesteadBlock": 0,
-        #     "eip150Block": 0,
-        #     "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-        #     "eip155Block": 0,
-        #     "eip158Block": 0,
-        #     "byzantiumBlock": 0,
-        #     "constantinopleBlock": 0,
-        #     "petersburgBlock": 0,
-        #     "istanbulBlock": 0,
-        #     "berlinBlock": 0,
-        #     "londonBlock": 0,
-        #     "terminalBlockHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-        #     "mergeNetsplitBlock": self.etb_config.get("merge-fork-block"),
-        #     "terminalTotalDifficulty": self.etb_config.get("terminal-total-difficulty"),
-        # }
-        # self.genesis["config"] = config
-        #
-        # if self.etb_config.get("clique-enabled"):
-        #     signers = "".join(s for s in self.etb_config.get("clique-signers"))
-        #     extradata = f"0x{'0' * 64}{signers}{'0' * 130}"
-        #     self.genesis["extraData"] = extradata
-        #     self.genesis["config"]["clique"] = {
-        #         "period": self.etb_config.get("seconds-per-eth1-block"),
-        #         "epoch": self.etb_config.get("clique-epoch"),
-        #     }
-        #     self.genesis["config"]["consensus"] = "clique"
-        #
-        # return self.genesis
-
     def create_besu_genesis(self):
         # "baseFeePerGas": self.ec["base-fee-per-gas"],
         self.genesis = {
