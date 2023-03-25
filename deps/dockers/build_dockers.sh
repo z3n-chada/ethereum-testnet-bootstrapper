85550f919da973db8779edb8f624c33df8829b3c
anjana <anjana.ratnayake@antithesis.com>
2023-03-25 14:03:55 +0000

    wip

--- deps/dockers/build_dockers.sh
+++ deps/dockers/build_dockers.sh
@@ -1,26 +1,26 @@
 
 # build the builder first
-# cd base-images/ || exit
-# echo "Building base images."
-# BUILDKIT=1 docker build --registries-conf=`pwd`/../../../registries.conf -t etb-client-builder -f etb-client-builder.Dockerfile .
-# BUILDKIT=1 docker build --registries-conf=`pwd`/../../../registries.conf -t etb-client-runner -f etb-client-runner.Dockerfile .
+cd base-images/ || exit
+echo "Building base images."
+BUILDKIT=1 docker build --registries-conf=`pwd`/../../../registries.conf -t etb-client-builder -f etb-client-builder.Dockerfile .
+BUILDKIT=1 docker build --registries-conf=`pwd`/../../../registries.conf -t etb-client-runner -f etb-client-runner.Dockerfile .
 
 # ## els then cls
-# cd ../el/ || exit
-# echo "Building execution clients"
-# ./build_dockers.sh
+cd ../el/ || exit
+echo "Building execution clients"
+./build_dockers.sh
 
-# cd cl/ || exit
-# echo "Building consensus clients"
-# ./build_dockers.sh
+cd ../cl/ || exit
+echo "Building consensus clients"
+./build_dockers.sh
 
-cd fuzzers/ || exit
+cd ../fuzzers/ || exit
 echo "building fuzzers."
 ./build_dockers.sh
 
-# #
-# cd ../base-images/ || exit
-# echo "Merging all clients."
-# # currently mainnet configs have not been modified to support the new boostrapper
-# BUILDKIT=1 docker build --registries-conf=`pwd`/../../registries.conf --no-cache -t etb-all-clients:minimal -f etb-all-clients_minimal.Dockerfile .
-# BUILDKIT=1 docker build --registries-conf=`pwd`/../../registries.conf --no-cache -t etb-all-clients:minimal-fuzz -f etb-all-clients_minimal-fuzz.Dockerfile .
+#
+cd ../base-images/ || exit
+echo "Merging all clients."
+# currently mainnet configs have not been modified to support the new boostrapper
+BUILDKIT=1 docker build --registries-conf=`pwd`/../../registries.conf --no-cache -t etb-all-clients:minimal -f etb-all-clients_minimal.Dockerfile .
+BUILDKIT=1 docker build --registries-conf=`pwd`/../../registries.conf --no-cache -t etb-all-clients:minimal-fuzz -f etb-all-clients_minimal-fuzz.Dockerfile .
