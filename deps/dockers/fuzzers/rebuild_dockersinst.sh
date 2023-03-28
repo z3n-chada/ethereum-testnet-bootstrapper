f76cc12ab50d35c39601f699b7e97b3f21c99039
Médi-Rémi Hashim <mediremi.hashim@antithesis.com>
2023-03-28 15:36:41 +0100

    chore: remove unused files

--- deps/dockers/fuzzers/rebuild_dockersinst.sh
+++
@@ -1,8 +0,0 @@
-
-
-./build-tx-fuzzer.sh
-
-BUILDKIT=1 docker build --registries-conf=`pwd`/../../../../registries.conf --no-cache -t lighthouse:etb-minimal-fuzz -f lighthouse-fuzz_minimal.Dockerfile .
-BUILDKIT=1 docker build --registries-conf=`pwd`/../../../../registries.conf --no-cache -t prysm:etb-minimal-fuzz -f prysm-fuzz_minimal.Dockerfile .
-BUILDKIT=1 docker build --registries-conf=`pwd`/../../../registries.conf --no-cache -t geth:bad-block-creator -f geth_bad-block-creator-inst.Dockerfile .
-BUILDKIT=1 docker build --registries-conf=`pwd`/../../../registries.conf --no-cache -t prysm:evil-shapella -f prysm_evil-shapella.Dockerfile .
